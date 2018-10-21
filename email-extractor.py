200e0f3eb058ea2d9408395e7136ba9b61a849c8
Alexander Amzin <amzin@alex-alex.ru>
2018-10-21 23:53:21 +0300

    first commit

---
+++ email-extractor.py
@@ -0,0 +1,13 @@
+## вытаскивает 
+
+def extract_emails(st):
+    # вытаскивает почтовые адреса
+    print ("hello")
+
+extract_emails("m@mail.ru")
+
+with open('email-sample.lst') as f:
+    read_data = f.read()
+f.closed
+
+print (read_data)
