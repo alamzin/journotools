32438f9607ef83b38651758c99a4aa418de89bcd
alamzin <amzin@alex-alex.ru>
2018-11-10 00:04:08 +0300

    email-extractor works, added some emails to email-sample.lst

--- email-extractor.py
+++ email-extractor.py
@@ -1,19 +1,26 @@
-## extracts emails 
+# -*- coding: utf-8 -*-
+# 
+# extracts emails from a file
+# uses email-sample.lst if arguments not provided
+import re
+
 
 samplefile = "email-sample.lst"
 
 def extract_emails(st):
     # extracts emails from a line
-    print ("hello",st)
-
-extract_emails("m@mail.ru")
-
-
-
+    regexemail = r"[A-Za-z0-9\-\.\+]+\@[A-Za-z0-9\-\.\+]+\.[A-Za-z0-9]+"
+    matches = re.findall (regexemail, st)
+    for match in matches:
+        print (match)
 
 
-with open(samplefile) as f:
-        for line in f:
-            extract_emails(line)
-f.closed
+with open(samplefile,encoding="utf8") as f: # open file
+    content = f.readlines()                 # read lines
 
+content = [x.strip() for x in content]      # strip a line of spaces
+
+for line in content:
+    extract_emails (line)                        # line by line
+    # print (line, "→", extract_emails(line)) # extract emails
+f.closed                                    # close the file
